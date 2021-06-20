54ed6e016de77d131acbf2e13f0d1c38d8d9a9fe
Mercy Daramola <mercydaramola23@gmail.com>
2021-06-20 10:25:40 -0700

    prints all single digit numbers of base 10 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	for (int i = 0 ; i <= 9; i++)
-
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
