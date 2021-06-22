ee07a40015febb13e90646e473114f52fe280777
Mercy Daramola <mercydaramola23@gmail.com>
2021-06-22 06:47:58 -0700

    prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *main - function to print
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char ch, i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
