b83a82a366b61795aceb8619a4d1eaa0e1293ae4
Mercy Daramola <mercydaramola23@gmail.com>
2021-06-24 22:51:42 -0700

    function that draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
 /**
- * print_line - function that checks for uppercase character.
+ * print_line - function that prints line
  * @n: is the integer for the paramaters of my function
- * Return: 0
+ * Return: void
  */
 void print_line(int n)
 {
-	int a = 0;
+	int i;
 
 	if (n > 0)
 	{
-		while (a <= n)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
-			a++;
 		}
-	 _putchar('\n');
 	}
-	else
 	_putchar('\n');
 }
