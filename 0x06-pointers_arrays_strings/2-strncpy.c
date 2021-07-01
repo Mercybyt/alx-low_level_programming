9344c4d813f981d13e2a5a6e4e4531e318e08408
Mercy Daramola <mercydaramola23@gmail.com>
2021-07-01 04:50:55 -0700

    copy string using strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
-
-	while (a != n)
+	while (*src)
 	{
-		dest[b] = src[a];
-		b++;
-		a++;
-		if (src[a] == '\0')
-		{
-			break;
-		}
+		*dest = *src;
+		src++;
+		dest++;
 	}
-	while (b != n)
-		dest[b++] = '\0';
+	*dest = '\0';
 	return (dest);
 }
