690a9e09a6f56bb461aa84f43be0748fe4d33a04
Mercy Daramola <mercydaramola23@gmail.com>
2021-07-01 06:21:05 -0700

    function that compared string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,22 @@
 #include "holberton.h"
+
 /**
  * _strcmp - function that compares two strings
  * @s2: paramater for string 2
  * @s1: paramater for string 1
  * Return: Always 0.
  */
-int _strcmp(char *s1, char *s2)
+	int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (s1[a] != '\0')
 	{
-		if (*s1 != *s2)
-		{
-			a = 1;
-		}
-		s1++;
-		s2++;
+		if (s1[a] < s2[a])
+			return (s1[a] - s2[a]);
+		if (s1[a] > s2[a])
+			return (s1[a] - s2[a]);
+		a++;
 	}
-	if (a == 1)
-		return (0);
-	else
-		return (1);
+	return (0);
 }
